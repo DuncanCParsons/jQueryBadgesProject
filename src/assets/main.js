6cea77fcff4b7a381d5eed6548270ad79b60b358
Duncan Parsons <duncanparsons@selenity.com>
2017-12-15 14:49:42 +0000

    Added code to get coures

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
   // your code will go here
+  
+  
+  // https://www.codeschool.com/users/2814713.json
+  
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2814713.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      $(response.courses.completed).each(function(index,item){
+        $()
+        $('#badges').append('<div class="course"><h3>' + item.title + '</h3><img src=' + item.badge + '/><a href=' + item.url+ ' target="_blank" class="btn btn-primary" Text="See Course" >See Course</a> '); 
+      })
+    }
+  });
 
 });
